40f536f5091cb162435546b2ae2a221afef199db
Rafael Ribeiro <rmcn.ribeiro@poli.ufrj.br>
2022-07-22 11:57:37 -0300

    implementa requisicao com novo BD

--- conexaoFirebase.py
+++ conexaoFirebase.py
@@ -27,43 +27,127 @@ def getDados(coluna):
 
     return str(toCpp)
 
+def getDadosTemp():
 
-# Funcao recebe o nome de uma coluna da base e retorna os 
-# valores contidos nesta.
-def getDadosColuna(coluna):
+    req= requests.get("https://trab-lingprog-default-rtdb.firebaseio.com/Temperatura.json")
 
-    # Realiza requisicao dos dados na web
-    reqResult = requests.get('https://trab-lingprog-default-rtdb.firebaseio.com/Dados.json')
-
-    # Checa se a requisicao foi completada com sucesso (status code 200).
-    # Caso contrario, o programa sai com codigo de erro '1'
-    if (reqResult.status_code != 200):
+    if (req.status_code != 200):
         print("Erro de requisicao.")
         exit(1)
     else:
         print("Requisicao realizada com sucesso.")
 
-    data = pd.read_json(reqResult.text)
+    data = pd.read_json(req.text)
     data["id"] = data.index
 
-    id = data["id"].values
-    hora = data["hora"].values
-    colunaDados = data[coluna].values
+    id = data["id"]
+    timestamp = data["timestamp"].values
+    temp = data["temperatura"].values
 
-    result = "" 
+    print(timestamp)
+    print(temp)
+
+    result = ""
+
     for index in range(len(data)):
+    
+        strParcial = str(id[index])+","+ str(timestamp[index])+","+ str(temp[index])
         
-        strParcial = str(id[index])+","+ str(hora[index])+","+ str(colunaDados[index])
-       
         if (index==0):
             result = strParcial
         else:
         
             resultTemp = result
-            strParcial = str(id[index])+","+ str(hora[index])+","+ str(colunaDados[index])
+            strParcial = str(id[index])+","+ str(timestamp[index])+","+ str(temp[index])
             result = resultTemp + ";" + strParcial
 
+    print(result)
+
     return result
+
+def getDadosPorta():
+
+    req= requests.get("https://trab-lingprog-default-rtdb.firebaseio.com/StatusPorta.json")
+
+    if (req.status_code != 200):
+        print("Erro de requisicao.")
+        exit(1)
+    else:
+        print("Requisicao realizada com sucesso.")
+
+    data = pd.read_json(req.text)
+    data["id"] = data.index
+
+    id = data["id"]
+    timestamp = data["timestamp"].values
+    porta = data["aberta"].values
+
+    print(timestamp)
+    print(porta)
+
+    result = ""
+
+    for index in range(len(data)):
+    
+        strParcial = str(id[index])+","+ str(timestamp[index])+","+ str(porta[index])
+        
+        if (index==0):
+            result = strParcial
+        else:
+        
+            resultTemp = result
+            strParcial = str(id[index])+","+ str(timestamp[index])+","+ str(porta[index])
+            result = resultTemp + ";" + strParcial
+
+    print(result)
+    
+    return result
+
+
+getDadosTemp()
+getDadosPorta()
+
+
+
+
+
+
+# # Funcao recebe o nome de uma coluna da base e retorna os 
+# # valores contidos nesta.
+# def getDadosColuna(coluna):
+
+#     # Realiza requisicao dos dados na web
+#     reqResult = requests.get('https://trab-lingprog-default-rtdb.firebaseio.com/Dados.json')
+
+#     # Checa se a requisicao foi completada com sucesso (status code 200).
+#     # Caso contrario, o programa sai com codigo de erro '1'
+#     if (reqResult.status_code != 200):
+#         print("Erro de requisicao.")
+#         exit(1)
+#     else:
+#         print("Requisicao realizada com sucesso.")
+
+#     data = pd.read_json(reqResult.text)
+#     data["id"] = data.index
+
+#     id = data["id"].values
+#     hora = data["hora"].values
+#     colunaDados = data[coluna].values
+
+#     result = "" 
+#     for index in range(len(data)):
+        
+#         strParcial = str(id[index])+","+ str(hora[index])+","+ str(colunaDados[index])
+       
+#         if (index==0):
+#             result = strParcial
+#         else:
+        
+#             resultTemp = result
+#             strParcial = str(id[index])+","+ str(hora[index])+","+ str(colunaDados[index])
+#             result = resultTemp + ";" + strParcial
+
+#     return result
 
 
 # Utilizado em teste02.cpp
